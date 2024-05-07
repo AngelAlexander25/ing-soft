cc6a7767a69c18d700a7ec9f248127380bae0ffd
Orbelin Jimenez <200300671@ucaribe.edu.mx>
2024-05-07 14:50:49 -0500

    se agregaron todos los cambios

--- js/style.js
+++ js/style.js
@@ -1,11 +1,14 @@
-window.addEventListener('scroll', function() {
-    var bar = document.querySelector('.bar');
-    var scrolled = window.scrollY;
-  
-    if (scrolled > 0) {
-      bar.classList.add('bar-scrolled');
-    } else {
-      bar.classList.remove('bar-scrolled');
-    }
-  });
-  
+window.addEventListener("scroll", function () {
+  var bar = document.querySelector(".bar");
+  var scrolled = window.scrollY;
+
+  if (scrolled > 0) {
+    // Aumenta el tamaño de la barra cuando el usuario desplaza hacia abajo
+    bar.style.height = "50px"; // Puedes ajustar el valor según tus necesidades
+    bar.classList.add("bar-scrolled");
+  } else {
+    // Restaura el tamaño original de la barra cuando el usuario está en la parte superior de la página
+    bar.style.height = "30px"; // Puedes ajustar el valor según tus necesidades
+    bar.classList.remove("bar-scrolled");
+  }
+});
