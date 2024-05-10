5efb837e2cf766c77520a970ffffc45ea4037914
Orbelin Jimenez <200300671@ucaribe.edu.mx>
2024-05-09 22:13:15 -0500

    Se agregaron nuevas imagenes

--- js/script.js
+++ js/script.js
@@ -2,14 +2,25 @@
 window.addEventListener("scroll", function () {
   var bar = document.querySelector(".bar");
   var scrolled = window.scrollY;
+  var links = document.querySelectorAll(".nav-link");
 
   if (scrolled > 0) {
     // Aumenta el tamaño de la barra cuando el usuario desplaza hacia abajo
     bar.style.height = "50px"; // Puedes ajustar el valor según tus necesidades
     bar.classList.add("bar-scrolled");
+
+    // Cambia el color de los enlaces
+    links.forEach(function (link) {
+      link.classList.add("scrolled");
+    });
   } else {
     // Restaura el tamaño original de la barra cuando el usuario está en la parte superior de la página
     bar.style.height = "30px"; // Puedes ajustar el valor según tus necesidades
     bar.classList.remove("bar-scrolled");
+
+    // Restaura el color original de los enlaces
+    links.forEach(function (link) {
+      link.classList.remove("scrolled");
+    });
   }
 });
